236534cc7c20a6f11b09fc807935ab9111b25881
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-03 19:44:32 -0300

    Create the QuestionsController

---
+++ BaoProvaAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaoProvaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpGet("api-life")]
+        public IActionResult Verify()
+        {
+            return Ok("API is running");
+        }
+    }
+}
