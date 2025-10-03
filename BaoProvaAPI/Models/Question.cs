44e702bbf84a4e3ca0ce10421a68f54ac1fc62ed
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-03 19:38:12 -0300

    Create the Question class in Models

---
+++ BaoProvaAPI/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace BaoProvaAPI.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public required string Statement { get; set; }
+        public string[] Alternatives { get; set; } = [];
+        public int CorrectAlternative { get; set; }
+        public string? Category { get; set; }
+        public int Year { get; set; }
+    }
+}
