d4d9438d6f0e7e9b38bc26c6963da6b3efb5ca5d
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-03 20:44:35 -0300

    Create the UserData

---
+++ BaoProvaAPI/Models/UserData.cs
@@ -0,0 +1,10 @@
+﻿namespace BaoProvaAPI.Models
+{
+    public class UserData
+    {
+        public int UserId { get; set; }
+        public int QuestionId { get; set; }
+        public int SelectedAlternative { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
