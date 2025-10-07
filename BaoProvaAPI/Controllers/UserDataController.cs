4f1fab3e06128605870ebef23b9ced7cc64e7dc9
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-07 17:59:20 -0300

    Create the UserDataController and the endpoint to safe UserData

---
+++ BaoProvaAPI/Controllers/UserDataController.cs
@@ -0,0 +1,33 @@
+﻿using BaoProvaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BaoProvaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserDataController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SaveUserData([FromBody] UserData userData)
+        {
+            var filePath = "Data/userdata.json";
+            List<UserData> userDataList = new();
+
+            if (System.IO.File.Exists(filePath))
+            {
+                var existingJson = System.IO.File.ReadAllText(filePath);
+                if (!string.IsNullOrWhiteSpace(existingJson))
+                {
+                    userDataList = JsonSerializer.Deserialize<List<UserData>>(existingJson) ?? new List<UserData>();
+                }
+            }
+
+            userDataList.Add(userData);
+            var updatedJson = JsonSerializer.Serialize(userDataList, new JsonSerializerOptions { WriteIndented = true });
+            System.IO.File.WriteAllText(filePath, updatedJson);
+
+            return Ok(new { message = "Dados do usuário salvos com sucesso." });
+        }
+    }
+}
