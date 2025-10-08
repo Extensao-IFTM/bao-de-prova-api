c6dcd74f91bb8468ad8d9fcd80c7dc010972c04e
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-08 18:39:58 -0300

    Add the Required Atributte in User model

--- BaoProvaAPI/Models/User.cs
+++ BaoProvaAPI/Models/User.cs
@@ -1,9 +1,15 @@
-﻿namespace BaoProvaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaoProvaAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
         public string Email { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
