5d41c8002ca73aa86106a939d5c211e4b6fffc95
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-08 19:23:10 -0300

    Add the Required Atributte in UserData model

--- BaoProvaAPI/Models/UserData.cs
+++ BaoProvaAPI/Models/UserData.cs
@@ -1,10 +1,19 @@
-﻿namespace BaoProvaAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaoProvaAPI.Models
 {
     public class UserData
     {
+        [Required(ErrorMessage = "UserId é obrigatório")]
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "QuestionId é obrigatório")]
         public int QuestionId { get; set; }
+
+        [Required(ErrorMessage = "SelectedAlternative é obrigatório")]
+        [Range(0, 4, ErrorMessage = "SelectedAlternative deve estar entre 0 e 4")]
         public int SelectedAlternative { get; set; }
+
         public bool IsCorrect { get; set; }
         public DateTime Timestamp { get; set; }
     }
