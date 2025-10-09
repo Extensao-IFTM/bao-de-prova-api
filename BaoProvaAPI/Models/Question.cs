50984f17d659b2dc47c3060b294abcc82a12306b
Lucas Emmanuel <lucasemmanuelrodrigues737@gmail.com>
2025-10-08 22:33:40 -0300

    Add the JsonPropertyName in Question model

--- BaoProvaAPI/Models/Question.cs
+++ BaoProvaAPI/Models/Question.cs
@@ -1,15 +1,28 @@
-﻿using System.Runtime.InteropServices.Marshalling;
+﻿using System.Text.Json.Serialization;
 
 namespace BaoProvaAPI.Models
 {
     public class Question
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        
+        [JsonPropertyName("statement")]
         public required string Statement { get; set; }
+
+        [JsonPropertyName("alternatives")]
         public string[] Alternatives { get; set; } = [];
+
+        [JsonPropertyName("correctAlternative")]
         public int CorrectAlternative { get; set; }
-        public required string Explanation {get; set; }
+
+        [JsonPropertyName("explanation")]
+        public string? Explanation {get; set; }
+
+        [JsonPropertyName("category")]
         public string? Category { get; set; }
+
+        [JsonPropertyName("year")]
         public int Year { get; set; }
     }
 }
